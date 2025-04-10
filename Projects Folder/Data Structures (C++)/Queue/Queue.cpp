37273222b36388a5b809f65a66b36bb3a197d1f3
Jed N. Victoria <134795258+JedEatSnax@users.noreply.github.com>
2025-04-10 21:32:29 +0800

    Update Queue.cpp

--- Projects Folder/Data Structures (C++)/Queue/Queue.cpp
+++ Projects Folder/Data Structures (C++)/Queue/Queue.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <queue>
+#include <windows.h>
+#include <psapi.h>
+
 using namespace std;
 
 void showQueue(queue<int> myQueue) {
@@ -10,6 +13,12 @@ void showQueue(queue<int> myQueue) {
     cout << endl;
 }
 
+void printMemoryUsage() {
+    PROCESS_MEMORY_COUNTERS pmc;
+    GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+    std::cout << "Peak RAM used: " << pmc.PeakWorkingSetSize / 1024 << " KB\n";
+}
+
 int main() {
     queue<int> myQueue;
     myQueue.push(10);
@@ -17,14 +26,14 @@ int main() {
     myQueue.push(30);
     myQueue.push(40);
     myQueue.push(50);
-    
+
     queue<int> myQueue2;
     myQueue2.push(100);
     myQueue2.push(200);
     myQueue2.push(300);
 
     cout << "Initial myQueue: ";
-    showQueue(myQueue);  
+    showQueue(myQueue);
 
     cout << "myQueue size: " << myQueue.size() << endl;
     cout << "myQueue front: " << myQueue.front() << endl;
@@ -32,12 +41,14 @@ int main() {
 
     myQueue.swap(myQueue2);
     cout << "After swapping the contents of myQueue and myQueue2:" << endl;
-    cout << "myQueue front: " << myQueue.front() << endl;    
-    cout << "myQueue2 front: " << myQueue2.front() << endl; 
+    cout << "myQueue front: " << myQueue.front() << endl;
+    cout << "myQueue2 front: " << myQueue2.front() << endl;
 
     myQueue.pop();
     cout << "After popping the front element from myQueue, it is now: ";
     showQueue(myQueue);
+
+    printMemoryUsage();
 
     return 0;
 }
